14a3097e754170503597f423b91323eeff1240bb
Chris Gaona <thebeloved88@sbcglobal.net>
2016-06-16 08:44:36 -0700

    Adds gulp build task & adds build task to default to simply run gulp

--- gulpfile.js
+++ gulpfile.js
@@ -13,7 +13,7 @@ var gulp = require('gulp'),
 
 // As a developer, I should be able to run the gulp scripts command at the command line to concatenate, minify, and copy all of the project’s JavaScript files into an all.min.js file that is then copied to the dist/scripts folder.
 gulp.task('concatScripts', function () {
-  gulp.src([
+  return gulp.src([
     'js/global.js',
     'js/circle/autogrow.js',
     'js/circle/circle.js'])
@@ -24,7 +24,7 @@ gulp.task('concatScripts', function () {
 });
 
 gulp.task('scripts', ['concatScripts'], function () {
-  gulp.src('js/all.js')
+  return gulp.src('js/all.js')
   .pipe(uglify())
   .pipe(rename('all.min.js'))
   .pipe(gulp.dest('dist/scripts'));
@@ -32,7 +32,7 @@ gulp.task('scripts', ['concatScripts'], function () {
 
 // As a developer, I should be able to run the gulp styles command at the command line to compile the project’s SCSS files into CSS, then concatenate and minify into an all.min.css file that is then copied to the dist/styles folder.
 gulp.task('compileSass', function () {
-  gulp.src('sass/global.scss')
+  return gulp.src('sass/global.scss')
   .pipe(maps.init())
   .pipe(sass())
   .pipe(rename('all.css'))
@@ -41,7 +41,7 @@ gulp.task('compileSass', function () {
 });
 
 gulp.task('styles', ['compileSass'], function () {
-  gulp.src('css/all.css')
+  return gulp.src('css/all.css')
   .pipe(cssmin())
   .pipe(rename('all.min.css'))
   .pipe(gulp.dest('dist/styles'));
@@ -49,11 +49,17 @@ gulp.task('styles', ['compileSass'], function () {
 
 // As a developer, I should be able to run the gulp images command at the command line to optimize the size of the project’s JPEG and PNG files, and then copy those optimized images to the dist/content folder.
 gulp.task('images', function () {
-  gulp.src('images/*')
+  return gulp.src('images/*')
   .pipe(imagemin())
   .pipe(gulp.dest('dist/content'))
 });
 
-gulp.task('default', function () {
-  console.log('This is the default task');
-});
+// As a developer, I should be able to run the gulp clean command at the command line to delete all of the files and folders in the dist folder.
+// TODO: 
+
+// As a developer, I should be able to run the gulp build command at the command line to run the clean, scripts, styles, and images tasks with confidence that the clean task completes before the other commands.
+// TODO:
+gulp.task('build', ['scripts', 'styles', 'images']);
+
+// As a developer, I should be able to run the gulp command at the command line to run the “build” task.
+gulp.task('default', ['build']);
