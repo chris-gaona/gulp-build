7e1e1e3b15c0efb38fb719c2fedd2101373c92d2
Chris Gaona <thebeloved88@sbcglobal.net>
2016-06-15 20:09:23 -0700

    Changes gulp scripts task back...adds all.js to /js & all.min.js to dist/scripts/

--- gulpfile.js
+++ gulpfile.js
@@ -10,12 +10,17 @@ var gulp = require('gulp'),
 // As a developer, when I run the gulp scripts or gulp styles commands at the command line, source maps are generated for the JavaScript and CSS files respectively.
 
 // As a developer, I should be able to run the gulp scripts command at the command line to concatenate, minify, and copy all of the project’s JavaScript files into an all.min.js file that is then copied to the dist/scripts folder.
-gulp.task('scripts', function () {
+gulp.task('concatScripts', function () {
   gulp.src([
     'js/global.js',
     'js/circle/autogrow.js',
     'js/circle/circle.js'])
   .pipe(concat('all.js'))
+  .pipe(gulp.dest('js'));
+});
+
+gulp.task('scripts', ['concatScripts'], function () {
+  gulp.src('js/all.js')
   .pipe(uglify())
   .pipe(rename('all.min.js'))
   .pipe(gulp.dest('dist/scripts'));
