bd8d9a124562953edb665b9a54cfb4e38643b23f
Chris Gaona <thebeloved88@sbcglobal.net>
2016-06-17 11:44:34 -0700

    Adds all comments; removes files gulp creates

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 'use strict';
 
+//rquires all needed npm modules
 var gulp = require('gulp'),
     concat = require('gulp-concat'),
     uglify = require('gulp-uglify'),
@@ -13,69 +14,101 @@ var gulp = require('gulp'),
     connect = require('gulp-connect'),
     inject = require('gulp-inject');
 
-// As a developer, when I run the gulp scripts command at the command line, all of my project’s JavaScript files will be linted using ESLint and if there’s an error, the error will output to the console and the build process will be halted.
+// run the gulp lint command at the command line
 gulp.task('lint', function () {
+  // all of the project’s JavaScript files will be linted using ESLint
+  // except for the node_modules folder
+  // return keyword is useful for when using the task as a dependency in
+  // another task
   return gulp.src(['js/**/*.js', '!node_modules/**'])
+  // always require semicolons where applicable
   .pipe(eslint({
     'rules':{
       'semi': [1, 'always']
     }
   }))
+  // and if there’s an error, the error will output to the console
   .pipe(eslint.format())
+  // and the build process will be halted.
   .pipe(eslint.failOnError());
 });
 
-// As a developer, when I run the gulp scripts or gulp styles commands at the command line, source maps are generated for the JavaScript and CSS files respectively.
-
-// As a developer, I should be able to run the gulp scripts command at the command line to concatenate, minify, and copy all of the project’s JavaScript files into an all.min.js file that is then copied to the dist/scripts folder.
+// run the gulp concatScripts command at the command line
+// uses lint task as a dependency to be run first
 gulp.task('concatScripts', ['lint'], function () {
   return gulp.src([
     'js/global.js',
     'js/circle/autogrow.js',
     'js/circle/circle.js'])
+  // generates source maps
   .pipe(maps.init())
+  // concatinates all js files
   .pipe(concat('all.js'))
+  // writes source maps
   .pipe(maps.write('./'))
+  // uses js folder as destination
   .pipe(gulp.dest('js'))
+  // calls reload if user is running gulp serve for livereload
   .pipe(connect.reload());
 });
 
+// run the gulp scripts command at the command line
+// uses concatScripts task as a dependency to be run first
 gulp.task('scripts', ['concatScripts'], function () {
   return gulp.src('js/all.js')
+  // minifies js file
   .pipe(uglify())
+  // renames file to the following as minified file
   .pipe(rename('all.min.js'))
+  // uses js folder as destination
   .pipe(gulp.dest('js'))
+  // copies minified file to dist/scripts folder
   .pipe(gulp.dest('dist/scripts'));
 });
 
-// As a developer, I should be able to run the gulp styles command at the command line to compile the project’s SCSS files into CSS, then concatenate and minify into an all.min.css file that is then copied to the dist/styles folder.
+// run the gulp compileSass command at the command line
 gulp.task('compileSass', function () {
   return gulp.src('sass/global.scss')
+  // generates source maps
   .pipe(maps.init())
+  // compiles sass files to css
   .pipe(sass())
+  // renames file to the following
   .pipe(rename('all.css'))
+  // writes source maps
   .pipe(maps.write('./'))
+  // uses css folder as destination
   .pipe(gulp.dest('css'))
+  // calls reload if user is running gulp serve for livereload
   .pipe(connect.reload());
 });
 
+// run the gulp styles command at the command line
+// uses compileSass task as a dependency to be run first
 gulp.task('styles', ['compileSass'], function () {
   return gulp.src('css/all.css')
+  // minifies css file
   .pipe(cssmin())
+  // renames css file to the following
   .pipe(rename('all.min.css'))
+  // uses css folder as the destination
   .pipe(gulp.dest('css'))
+  // copies css file to dist/styles folder
   .pipe(gulp.dest('dist/styles'));
 });
 
-// As a developer, I should be able to run the gulp images command at the command line to optimize the size of the project’s JPEG and PNG files, and then copy those optimized images to the dist/content folder.
+// run the gulp images command at the command line to
 gulp.task('images', function () {
   return gulp.src('content/*')
+  // optimizes the size of the project’s JPEG and PNG files
   .pipe(imagemin())
+  // copies those optimized images to the dist/content folder
   .pipe(gulp.dest('dist/content'));
 });
 
-// The gulp serve command builds and serves the project using a local web server.
+// gulp serve command builds and serves the project using a local web server.
 gulp.task('connect', function() {
+  // specifies port 3000 and makes livereload true
   connect.server({
     port: 3000,
     // root: 'dist',
@@ -85,37 +118,51 @@ gulp.task('connect', function() {
 
 // watch for changes to sass & js files & automatically compile/concat
 gulp.task('watch', function () {
-  gulp.watch(['sass/**/*.scss', 'sass/**/*.sass'], ['compileSass']);
-  gulp.watch(['js/**/*.js'], ['concatScripts']);
+  gulp.watch(['sass/**/*.scss', 'sass/**/*.sass'], ['styles']);
+  gulp.watch(['js/**/*.js'], ['scripts']);
 });
 
-// As a developer, I should be able to run the gulp clean command at the command line to delete all of the files and folders in the dist folder.
+// run the gulp clean command at the command line
+// to delete all of the files and folders in the dist folder
 gulp.task('clean', function() {
   //delete dist folder contents before rebuilding
-  del(['dist', 'css/all*.css*', 'js/all*.js*']);
+  return del(['dist', 'css/all*.css*', 'js/all*.js*']);
 });
 
-// As a developer, I should be able to run the gulp build command at the command line to run the clean, scripts, styles, and images tasks with confidence that the clean task completes before the other commands.
+// run the gulp build command at the command line
+// uses clean task as a dependency so it is ran first
 gulp.task('build', ['clean'], function () {
+  // after clean task has ran....run scripts, styles, and images tasks
   gulp.start(['scripts', 'styles', 'images']);
   //{base:'./'} makes it so the build in dist uses
   //the proper directories from dev environment
   return gulp.src(['index.html', 'icons/**'], {base:'./'})
+  // adds other needed files to dist folder
   .pipe(gulp.dest('dist'));
 });
 
+// run the gulp inject command at the command line
+// used to add new script tags for production ready index.html
 gulp.task('inject', function () {
+  // define target
   var target = gulp.src('./dist/index.html');
   // It's not necessary to read the files (will speed up things), we're only after their paths:
-  var sources = gulp.src(['js/all.min.js', 'css/all.min.css'], {read: false});
+  // define source files
+  // injects new script tags into the index.html in the dist folder
+  // sets relative option to true to remove the 'dist' part of the script tag
   return target.pipe(inject(gulp.src(['./dist/**/*.js', './dist/**/*.css'], {read: false}), {relative: true}))
   .pipe(gulp.dest('./dist'));
 });
 
-// As a developer, I should be able to run the gulp command at the command line to run the “build” task.
+// run the gulp command at the command line to run the “build” task.
 gulp.task('default', ['build'], function () {
+  // after build task is complete run the inject task
   gulp.start(['inject']);
 });
 
-// When running the gulp serve command, the scripts task is run and the current page is reloaded in the browser when a change is made to any JavaScript (*.js) file.
-gulp.task('serve', ['build', 'connect', 'watch']);
+// when running the gulp serve command, the scripts task is run
+// and the current page is reloaded in the browser when a change
+// is made to any JavaScript (*.js) file or sass files
+gulp.task('serve', ['scripts', 'styles'], function () {
+  gulp.start(['connect', 'watch']);
+});
