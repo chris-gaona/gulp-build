d7612e59bed3dd61b5ebb3d0f82659609e1fc6f9
Chris Gaona <thebeloved88@sbcglobal.net>
2016-06-17 09:27:52 -0700

    Adds gulp serve task to create server & live reload; uses gulp-inject to change scripts in html

--- gulpfile.js
+++ gulpfile.js
@@ -10,7 +10,8 @@ var gulp = require('gulp'),
     imagemin = require('gulp-imagemin'),
     del = require('del'),
     eslint = require('gulp-eslint'),
-    connect = require('gulp-connect');
+    connect = require('gulp-connect'),
+    inject = require('gulp-inject');
 
 // As a developer, when I run the gulp scripts command at the command line, all of my project’s JavaScript files will be linted using ESLint and if there’s an error, the error will output to the console and the build process will be halted.
 gulp.task('lint', function () {
@@ -43,6 +44,7 @@ gulp.task('scripts', ['concatScripts'], function () {
   return gulp.src('js/all.js')
   .pipe(uglify())
   .pipe(rename('all.min.js'))
+  .pipe(gulp.dest('js'))
   .pipe(gulp.dest('dist/scripts'));
 });
 
@@ -61,20 +63,22 @@ gulp.task('styles', ['compileSass'], function () {
   return gulp.src('css/all.css')
   .pipe(cssmin())
   .pipe(rename('all.min.css'))
+  .pipe(gulp.dest('css'))
   .pipe(gulp.dest('dist/styles'));
 });
 
 // As a developer, I should be able to run the gulp images command at the command line to optimize the size of the project’s JPEG and PNG files, and then copy those optimized images to the dist/content folder.
 gulp.task('images', function () {
-  return gulp.src('images/*')
+  return gulp.src('content/*')
   .pipe(imagemin())
-  .pipe(gulp.dest('dist/content'))
+  .pipe(gulp.dest('dist/content'));
 });
 
 // The gulp serve command builds and serves the project using a local web server.
 gulp.task('connect', function() {
   connect.server({
     port: 3000,
+    // root: 'dist',
     livereload: true
   });
 });
@@ -84,9 +88,6 @@ gulp.task('watch', function () {
   gulp.watch(['sass/**/*.scss', 'sass/**/*.sass'], ['compileSass']);
   gulp.watch(['js/**/*.js'], ['concatScripts']);
 });
-
-// When running the gulp serve command, the scripts task is run and the current page is reloaded in the browser when a change is made to any JavaScript (*.js) file.
-gulp.task('serve', ['connect', 'watch']);
 
 // As a developer, I should be able to run the gulp clean command at the command line to delete all of the files and folders in the dist folder.
 gulp.task('clean', function() {
@@ -103,6 +104,19 @@ gulp.task('build', ['clean'], function () {
   .pipe(gulp.dest('dist'));
 });
 
+gulp.task('inject', function () {
+  var target = gulp.src('./dist/index.html');
+  // It's not necessary to read the files (will speed up things), we're only after their paths:
+  var sources = gulp.src(['js/all.min.js', 'css/all.min.css'], {read: false});
+  return target.pipe(inject(gulp.src(['./dist/**/*.js', './dist/**/*.css'], {read: false}), {relative: true}))
+  .pipe(gulp.dest('./dist'));
+});
+
 // As a developer, I should be able to run the gulp command at the command line to run the “build” task.
 // gulp.task('default', gulp.series('clean', 'build'));
-gulp.task('default', ['build']);
+gulp.task('default', ['build'], function () {
+  gulp.start(['inject']);
+});
+
+// When running the gulp serve command, the scripts task is run and the current page is reloaded in the browser when a change is made to any JavaScript (*.js) file.
+gulp.task('serve', ['build', 'connect', 'watch']);
